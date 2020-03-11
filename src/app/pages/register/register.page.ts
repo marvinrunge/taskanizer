a18fbfa863d2e8af93482724f49b90b540af2e4f
marvinrunge <marvinrunge@web.de>
2020-03-11 09:48:07 +0100

    login and register page

---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage {
+  public registerForm: FormGroup;
+
+  public matcher = new MyErrorStateMatcher();
+
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      username: new FormControl('', [Validators.required, Validators.maxLength(30)]),
+      password: new FormControl('', [Validators.required, Validators.maxLength(30)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.maxLength(30)])
+    }, { validator: this.checkPassword });
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.registerForm.controls[controlName].hasError(errorName);
+  }
+
+  register() {
+    return this.authService.register(
+      {
+        name: this.registerForm.get('username').value,
+        password: this.registerForm.get('password').value
+      }
+    );
+  }
+
+  checkPassword(group: FormGroup) {
+    const pass = group.get('password').value;
+    const confirmPass = group.get('confirmPassword').value;
+
+    console.log(pass + ':' + confirmPass);
+    return pass === confirmPass ? null : { notSame: true };
+  }
+}
