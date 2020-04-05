00aa6c5f408c7a58ab92f06db69c01a249ffa789
marvinrunge <marvinrunge@web.de>
2020-04-05 21:43:55 +0200

    remove unused

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { RootStoreState, TaskActions } from 'src/app/root-store';
 import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -12,7 +9,6 @@ import { Router } from '@angular/router';
 export class SettingsPage {
 
   constructor(
-    private store$: Store<RootStoreState.State>,
     private authService: AuthService
     ) {}
 
