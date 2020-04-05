1ee7f24362060445531b8271b636dc7a426e7dd4
marvinrunge <marvinrunge@web.de>
2020-04-05 21:49:35 +0200

    imprts

--- src/app/pages/settings/settings.page.spec.ts
+++ src/app/pages/settings/settings.page.spec.ts
@@ -6,7 +6,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { provideMockStore } from '@ngrx/store/testing';
-import { MaterialModule } from 'src/app/material.module';
 import { SharedModule } from 'src/app/shared.module';
 
 describe('SettingsPage', () => {
