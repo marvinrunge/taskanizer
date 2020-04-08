de733f57ef9365f2a6935e785a26b9d0d608bb71
marvinrunge <marvinrunge@web.de>
2020-04-08 18:08:31 +0200

    test fixes

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -2,9 +2,11 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { SettingsPage } from './settings.page';
 import { SharedModule } from 'src/app/shared.module';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   imports: [
+    IonicModule,
     SharedModule,
     RouterModule.forChild([{ path: '', component: SettingsPage }])
   ],
